9fc784a563d67d96010d0290b820d9f55e47c94d
Stephen Oney <swloney@gmail.com>
2015-11-29 23:20:47 -0500

    abc

--- js/mcquestion.js
+++ js/mcquestion.js
@@ -1,23 +1,41 @@
 function getOptions() {
-	return ['1', '2', '3', '4'];
+
 }
 
 $.widget('iss.mcquestion', {
 	options: {
-		
+		getQuestion: function() {
+			return $('<div />').text('This is the question');
+		},
+		getResponseOptions: function() {
+			function getOption(num, isCorrect) {
+				return {
+					serialize: function() {
+						return num;
+					},
+					getElement: function() {
+						return $('<div />').text(num);
+					},
+					correct: isCorrect
+				}
+			}
+
+			return[getOption('A', false), getOption('B', true), getOption('C', false), getOption('D', false)];
+		}
 	},
+
 	_create: function() {
 		this.questionContainer = $('<div />').addClass('question row')
 									.appendTo(this.element);
 
 		this.question = $('<div />').addClass('col-sm-12')
-									.text('question')
-									.appendTo(this.questionContainer);
+									.appendTo(this.questionContainer)
+									.append(this.option('getQuestion')());
 
 		this.optionsContainer = $('<div />').addClass('options row')
 											.appendTo(this.element);
 
-		this.submitContainer = $('<div />').addClass('row')
+		this.submitContainer = $('<div />').addClass('next row')
 											.appendTo(this.element);
 
 		this.submitButton = $('<button />').attr({
@@ -28,44 +46,74 @@ $.widget('iss.mcquestion', {
 			.on('click', $.proxy(this._onSubmit, this))
 			.append($('<span />').addClass('glyphicon glyphicon-chevron-right'));
 
-		$(window).on('keypress.submitanswer', $.proxy(this._onKeyPress, this));
 
 
-		this._updateOptions();
+		this.options = this.option('getResponseOptions')();
+
+		_.each(this.options, function(option, index) {
+			var optionDisplay = $('<div />').addClass('col-md-3 option')
+											.appendTo(this.optionsContainer)
+											.append($('<span />').text(index+1)
+																.addClass('number'))
+											.append(option.getElement());
+		}, this);
+
+		$('.option', this.optionsContainer).on('click.optionSelected', $.proxy(this._optionSelected, this));
+		this.lastFocus = this.startTime = (new Date()).getTime();
+		this.timeInFocus = 0;
+
+		$(window)	.on('keydown.submitanswer', $.proxy(this._onKeyPress, this))
+					.on('focus.stoptiming', $.proxy(this._onWindowFocus, this))
+					.on('blur.stoptiming', $.proxy(this._onWindowBlur, this));
+
+		this.element.on('focus')
 	},
 
 	_destroy: function() {
+		$('.option', this.optionsContainer).off('click.optionSelected');
+		$(window).off('keydown.submitanswer focus.stoptiming blur.stoptiming');
+		this.questionContainer.remove();
+		this.optionsContainer.remove();
+		this.submitContainer.remove();
+	},
 
+	_onWindowFocus: function() {
+		this.lastFocus = (new Date()).getTime();
+	},
+
+	_onWindowBlur: function() {
+		this.timeInFocus += (new Date()).getTime() - this.lastFocus;
 	},
 
 	_onKeyPress: function(event) {
 		var which = event.which;
 		if(which >= 49 && which <= 52) {
-			var selectionIndex = which - 49;
-			this._optionSelected({
-				target: this.optionsContainer.children().eq(selectionIndex)
-			});
-		} else if(which === 13) {
+			this._setSelectedIndex(which - 49);
+		} else if(which === 13) { // enter
 			this._onSubmit();
+		} else if(which === 39) { // right
+			if(_.isNumber(this.selectedIndex)) {
+				this._setSelectedIndex((this.selectedIndex+1)%this.options.length);
+			} else {
+				this._setSelectedIndex(0);
+			}
+		} else if(which === 37) { // left
+			if(_.isNumber(this.selectedIndex)) {
+				this._setSelectedIndex(this.selectedIndex === 0 ? this.options.length-1 : this.selectedIndex-1);
+			} else {
+				this._setSelectedIndex(this.options.length-1);
+			}
 		}
 	},
 
-	_updateOptions: function() {
-		this.options = getOptions();
-
-		_.each(this.options, function(option) {
-			var optionDisplay = $('<div />').addClass('col-md-3 option')
-											.appendTo(this.optionsContainer)
-											.text(option);
-		}, this);
-
-		$('.option', this.optionsContainer)
-			.off('click.optionSelected')
-			.on('click.optionSelected', $.proxy(this._optionSelected, this));
+	_setSelectedIndex: function(index) {
+		this._optionSelected({
+			target: this.optionsContainer.children().eq(index)
+		});
 	},
 
 	_optionSelected: function(event) {
-		var target = $(event.target);
+		var target = $(event.target).closest('.option');
 
 		this.selectedIndex = target.index();
 		this.selectedOption = this.options[this.selectedIndex];
@@ -78,7 +126,15 @@ $.widget('iss.mcquestion', {
 
 	_onSubmit: function() {
 		if(this.selectedOption) {
-			console.log(this.selectedOption);
+			this.timeInFocus += (new Date()).getTime() - this.lastFocus;
+
+			var event = $.Event('answered');
+			event.selectedOption = this.selectedOption.serialize();
+			event.correct = this.selectedOption.correct;
+			event.totalTime = (new Date()).getTime() - this.startTime;
+			event.focusTime = this.timeInFocus;
+
+			this.element.trigger(event);
 		}
 	}
 });
--- js/mcquiz.js
+++ js/mcquiz.js
@@ -21,24 +21,44 @@ $.widget('iss.mcquiz', {
 
 		this.questionContainer = $('<div />')	.addClass('container')
 												.appendTo(this.element)
-												.mcquestion();
+												.mcquestion()
+												.on('answered', $.proxy(this._onQuestionAnswered, this));
 
 		this._updateProgressBar();
 	},
 	_destroy: function() {
 
 	},
-	setOption: function(key, value) {
+	_onQuestionAnswered: function(event) {
+		var selectedOption = event.selectedOption;
+		var correct = event.correct;
+		var totalTime = event.totalTime;
+		var focusTime = event.focusTime;
+
+		this.option('currentQuestion', this.option('currentQuestion')+1);
+	},
+	_setOption: function(key, value) {
 		this._super(key, value);
 
 		if(key === 'currentQuestion' || key === 'numQuestions') {
 			this._updateProgressBar();
 		}
+
+		if(key === 'currentQuestion') {
+			this.questionContainer.mcquestion('destroy');
+
+			if(value > this.option('numQuestions')) {
+				this.questionContainer.text('Thank you');
+			} else {
+				this.questionContainer.mcquestion();
+			}
+		}
 	},
 	_updateProgressBar: function() {
 		var currentQuestion = this.option('currentQuestion'),
-			numQuestions = this.option('numQuestions'),
-			percentage = Math.round(100*currentQuestion / numQuestions);
+			numQuestions = this.option('numQuestions');
+		currentQuestion = Math.min(currentQuestion, numQuestions);
+		var percentage = Math.round(100*currentQuestion / numQuestions);
 
 		this.progressBar.css({
 			width: percentage + '%'
