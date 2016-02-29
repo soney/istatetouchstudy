213d615b321fec42bdb4e39f6570209d86e955c3
Steve Oney <swloney@gmail.com>
2016-02-29 14:00:04 -0500

    adding four new gestures

---
+++ gestures/implementations/primitives/two finger press hold to increment.js
@@ -0,0 +1,37 @@
+registerBehavior("tap", "primitives", function(TouchCluster, Path, fire, begin, update, end) {
+
+var touch = new TouchCluster({
+    numFingers: 1,
+    greedy: true
+});
+
+var MIN_TIME_MILLISECONDS = 500;
+var radius = 40;
+var validTouch = true;
+var timeoutID;
+
+
+var circle = new Path().circle(touch.getStartXConstraint(),
+                                touch.getStartYConstraint(),
+                                radius);
+                                
+touch.downInside = circle;
+
+touch.on('satisfied', function() {
+    validTouch = true; 
+    timeoutID = setTimeout(function() { 
+        validTouch = false;  
+    }, MIN_TIME_MILLISECONDS);
+});
+
+touch.on('cross', circle, function() {  
+    validTouch = false;  
+});
+
+touch.on('unsatisfied', function() {
+    if (validTouch) { 
+        fire();   
+    }
+});
+
+});
